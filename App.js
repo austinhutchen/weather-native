c0ac9d918846280474793e861131a4a397802687
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-01-08 06:27:13 -0800

    layout fix

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View, SafeAreaView, Button, Image} from 'react-native';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text >Weather</Text>
+    <Button title="ayo" onPress={()=>{
+      //do something
+    }} style={{color:'white',fontSize:20}}/>
+    <Image source={"./assets/adaptive-icon.png"}/>
+    </SafeAreaView>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    // flex is set to 1 to allow for resizing to fit screen
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'teal',
     alignItems: 'center',
     justifyContent: 'center',
   },
