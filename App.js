9d2b141d1481dbf284a88487fa1e557b92f77019
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-01-08 07:22:31 -0800

    formatting done!

--- App.js
+++ App.js
@@ -5,29 +5,35 @@ import {
   SafeAreaView,
   Button,
   Image,
+  TextInput,
 } from "react-native";
 
 export default function App() {
-  const handlePress = () => {
-    console.log("Text pressed");
+  const handlePress = (input) => {
+    console.log(input);
   };
   return (
     <SafeAreaView style={styles.container}>
       <Text style={"fontSize:32"}>Weather</Text>
       <Image source={require("./assets/favicon.png")} />
+      <TextInput style={styles.TextInput}
+      placeholder="Enter your city"
+      multiline={true}
+      autoCapitalize={true}
+      autoComplete={true}
+      ></TextInput>
       <Button
-        title="ayo"
+        title="Submit"
         onPress={() => {
           //do something
-          handlePress();
+          handlePress(TextInput);
         }}
         style={{ color: "black", fontSize: 20 }}
       />
-
     </SafeAreaView>
   );
 }
-
+// works as a css style sheet
 const styles = StyleSheet.create({
   container: {
     // flex is set to 1 to allow for resizing to fit screen
@@ -36,4 +42,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  TextInput:{
+    margin:20,
+    backgroundColor: "white",
+    borderWidth:2
+  }
 });
