35f25cfb20cf7bc0fc9341bdea5bf84e0f6390a8
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-01-08 06:34:20 -0800

    basic image

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-
-import { StyleSheet, Text, View, SafeAreaView, Button, Image} from 'react-native';
-
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Button,
+  Image,
+} from "react-native";
 
 export default function App() {
-  const handlePress=() =>{
+  const handlePress = () => {
     console.log("Text pressed");
-  }
+  };
   return (
     <SafeAreaView style={styles.container}>
-      <Text >Weather</Text>
-    <Button title="ayo" onPress={()=>{
-      //do something
-      handlePress();
-    }} style={{color:'white',fontSize:20}}/>
-    <Image source={"./assets/adaptive-icon.png"}/>
+      <Text style={"fontSize:32"}>Weather</Text>
+      <Image source={require("./assets/favicon.png")} />
+      <Button
+        title="ayo"
+        onPress={() => {
+          //do something
+          handlePress();
+        }}
+        style={{ color: "black", fontSize: 20 }}
+      />
+
     </SafeAreaView>
-    
   );
 }
 
@@ -23,8 +32,8 @@ const styles = StyleSheet.create({
   container: {
     // flex is set to 1 to allow for resizing to fit screen
     flex: 1,
-    backgroundColor: 'teal',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
