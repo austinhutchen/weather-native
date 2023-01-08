8c3a7f8ba5e80a8c7b40f4c712273e56daf6298a
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-01-08 06:29:01 -0800

    debugging

--- App.js
+++ App.js
@@ -3,11 +3,15 @@ import { StyleSheet, Text, View, SafeAreaView, Button, Image} from 'react-native
 
 
 export default function App() {
+  const handlePress=() =>{
+    console.log("Text pressed");
+  }
   return (
     <SafeAreaView style={styles.container}>
       <Text >Weather</Text>
     <Button title="ayo" onPress={()=>{
       //do something
+      handlePress();
     }} style={{color:'white',fontSize:20}}/>
     <Image source={"./assets/adaptive-icon.png"}/>
     </SafeAreaView>
